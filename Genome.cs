dfee79b4688d67300e0abf8251c1cd25344bd70e
hedgehog <myregisg@gmail.com>
2023-11-23 00:19:09 +0300

    Добавьте файлы проекта.

---
+++ Genome.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneticAlgoritm
+{
+    public class Genome
+    {
+        public List<int> Path { get; private set; }
+        public int PathWeight;
+        public Genome(List<int> Path, List<List<int>> Map)
+        {
+            this.Path = Path;
+            this.PathWeight = CalculatePath(Map);
+        }
+
+        private int CalculatePath(List<List<int>> map)
+        {
+            int totalWeight = 0;
+            for(int i = 1; i < Path.Count; i++)
+            {
+                if (map[Path[i - 1]][Path[i]] == 0)
+                    return int.MaxValue;
+                totalWeight += map[Path[i - 1]][Path[i]];
+            }
+            return totalWeight;
+        }
+
+        public Genome(Genome parent1, Genome parent2, List<List<int>> Map)
+        {
+            List<int> mask = new List<int>();
+            for (int i = 0; i < parent1.Path.Count - 2; i++)
+                mask.Add(Random.Shared.Next(0, 2));
+
+            this.Path = new List<int>(Enumerable.Repeat(0,parent1.Path.Count));
+            this.Path[0] = parent1.Path[0];
+            this.Path[parent1.Path.Count-1] = parent1.Path[parent1.Path.Count-1];
+
+            for(int i = 1; i < parent1.Path.Count-1; i++)
+            {
+                if (mask[i-1] == 1)
+                    this.Path[i] = parent1.Path[i];
+            }
+            for(int i = 1; i < parent2.Path.Count-1; i++)
+            {
+                if (mask[i-1] == 0 && !this.Path.Contains(parent2.Path[i]))
+                    this.Path[i] = parent2.Path[i];
+            }
+
+            for(int i = 1; i < this.Path.Count - 1;i++)
+            {
+                if (this.Path[i] == 0)
+                    this.Path[i] = Enumerable.Range(0,parent1.Path.Count).Where(x => !this.Path.Contains(x)).First();
+            }
+            this.PathWeight = CalculatePath(Map);
+        }
+        public void Mutate()
+        {
+            int leftBound = Random.Shared.Next(1, this.Path.Count - 2);
+            int rightBound = Random.Shared.Next(leftBound+1, this.Path.Count - 1);
+
+            int shuffleNum = rightBound-leftBound;
+            while(shuffleNum-- > 0)
+            {
+                int firstIdx = Random.Shared.Next(leftBound, rightBound + 1),
+                    secondIdx;
+                do
+                {
+                    secondIdx = Random.Shared.Next(leftBound, rightBound + 1);
+                }while(secondIdx == firstIdx);
+
+                int tmp = this.Path[firstIdx];
+                this.Path[firstIdx] = this.Path[secondIdx];
+                this.Path[secondIdx] = tmp;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿namespace GeneticAlgoritm
+{
+    internal class Program
+    {
+        static List<List<int>> Map = new List<List<int>>
+        {
+            new List<int> { 0,10,25,40,26,12 },
+            new List<int> { 10,0,20,30,28,18 },
+            new List<int> { 25,20,0,15,19,22 },
+            new List<int> { 40,30,15,0,8,21 },
+            new List<int> { 26,28,19,8,0,16 },
+            new List<int> { 12,18,22,21,16,0 }
+        };
+        const int PopulationSize = 20;
+        const int GenerationCount = 50;
+        static void Main(string[] args)
+        {
+            List<Genome> Population = new List<Genome>();
+
+            while(Population.Count < PopulationSize)
+            {
+                List<int> UnseenCities = new List<int>();
+                for(int i = 1; i < Map[0].Count; i++)
+                    UnseenCities.Add(i);
+
+                List<int> newPath = new List<int>() { 0 };
+                while(UnseenCities.Count > 0)
+                {
+                    int currentCity = Random.Shared.Next(0, UnseenCities.Count);
+                    newPath.Add(UnseenCities[currentCity]);
+                    UnseenCities.RemoveAt(currentCity);
+                }
+                newPath.Add(0);
+                Population.Add(new Genome(newPath, Map));
+            }
+            Console.WriteLine("Начальная популяция:");
+            Population.Sort((x, y) => x.PathWeight.CompareTo(y.PathWeight));
+            Population.ForEach(x =>
+            {
+                Console.Write(x.Path[0]);
+                foreach (int i in x.Path.Skip(1))
+                    Console.Write("->{0}", i);
+                Console.WriteLine(" = {0}", x.PathWeight);
+            });
+            Console.WriteLine();
+            int Generation = 0;
+            do
+            {
+                //Сортировка
+                Population.Sort((x, y) => x.PathWeight.CompareTo(y.PathWeight));
+
+                int newChildNum = Random.Shared.Next(1,10);
+                do {
+                    //Селекция
+                    Genome[] parents = Selection(Population);
+
+                    //Скрещивание
+                    Population.Add(new Genome(parents[0], parents[1], Map));
+
+                    //Удаление ненужных
+                    Population.RemoveRange(PopulationSize - 1, Population.Count - PopulationSize);
+                }while(newChildNum-- > 0);
+                Population.ForEach(i => i.Mutate());
+
+                Console.WriteLine("Среднее значение оценочной функции поколения #{0} = {1}", Generation + 1, Population.Average(i => i.PathWeight));
+            } while(Generation++ < GenerationCount);
+
+            Console.WriteLine();
+            Population.Sort((x, y) => x.PathWeight.CompareTo(y.PathWeight));
+            Console.WriteLine("Лучший потомок:");
+            Console.Write(Population[0].Path[0]);
+            foreach (int i in Population[0].Path.Skip(1))
+                Console.Write("->{0}", i);
+            Console.WriteLine(" = {0}", Population[0].PathWeight);
+        }
+        static Genome[] Selection(List<Genome> Population)
+        {
+            List<int[]> WeightSum = new List<int[]>() { new int[] { 0, Population[0].PathWeight } };
+            for(int i = 1; i < Population.Count; i++)
+                WeightSum.Add(new int[] { i, WeightSum[i - 1][1] + Population[i].PathWeight });
+
+            Genome? firstParent = null, secondParent = null;
+
+            do
+            {
+                int min = Random.Shared.Next(0, WeightSum[WeightSum.Count - 1][1] - 1);
+                firstParent = Population[WeightSum.Where(i => i[1] > min).Select(i => i[0]).First()];
+            } while (firstParent == null);
+
+            do
+            {
+                int min = Random.Shared.Next(0, WeightSum[WeightSum.Count - 1][1] - 1);
+                secondParent = Population[WeightSum.Where(i => i[1] > min).Select(i => i[0]).First()];
+            } while (secondParent == null || secondParent == firstParent);
+
+            return new Genome[2] { firstParent, secondParent };
+        }
+    }
+}
